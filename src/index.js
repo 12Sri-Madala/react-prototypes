cc942d63918e82d0b32d52d0d00e6d780a8a2d08
Sridhar Madala <Si.madala19@gmail.com>
2018-10-31 20:18:16 -0700

    changes for react intro prototype:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user = {
+    name: 'Steve' ,
+    luckyNumber: luckyNumber()
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+function luckyNumber(){
+    return Math.floor(Math.random() * 1000 + 1)
+}
+
+function greeting(user){
+    return (<div className = 'container'>
+        <h1>Welcome {user.name}! </h1>
+        <h2 className = 'text-muted'>{user.luckyNumber}</h2>
+        </div>
+    )
+    
+    
+}
+
